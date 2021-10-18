e6b1a2cd46ec7dd88e64108badd91e0dc6424858
romjanxr <romjanvr5@gmail.com>
2021-10-18 14:32:19 +0600

    first commit

---
+++ src/dummy.js
@@ -0,0 +1,38 @@
+const services = [
+    {
+        id: 1,
+        title: 'Therapiya',
+        img: 'https://i.ibb.co/hVmWnFQ/img1.jpg',
+        description: 'An initiative by Vana, Therapiya is a wellness center for ancient healing traditions, health and wellbeing. We created a symbol to represent the meeting point of the different therapies they offer – Ayurveda, Sowa Rigpa, Traditional Chinese Medicine and Yoga.',
+    },
+    {
+        id: 2,
+        title: 'Dentistry',
+        img: 'https://i.ibb.co/ZJ2cv9n/img2.jpg',
+        description: 'At MINT dentistry, our skilled dental professionals offer innovative preventive and general dental care for healthy, beautiful smiles. All of our Dallas, TX and Houston, TX area patients of every age can receive comprehensive dental treatments in order to detect and prevent further issues before they develop. Through regular dental exams and cleanings, you can experience improved overall dental health, contributing to restorative and aesthetic results.',
+    },
+    {
+        id: 3,
+        title: 'Virusology',
+        img: 'https://i.ibb.co/6gSTHGm/img3.jpg',
+        description: 'Virology is the scientific discipline concerned with the study of the biology of viruses and viral diseases, including the distribution, biochemistry, physiology, molecular biology, ecology, evolution and clinical aspects of viruses.',
+    },
+    {
+        id: 4,
+        title: 'Pharmocology',
+        img: 'https://i.ibb.co/m0Gbdp7/img4.jpg',
+        description: 'Pharmacology is a branch of science that deals with the study of drugs and their actions on living systems - that is, the study of how drugs work in the body (sometimes referred to as drug actions). To understand this we need to consider what a drug is, how it affects our physical, emotional and psychological wellbeing, the type of drug being used, the modes of administration, how the drug is absorbed and the characteristics of the person taking the drug.',
+    },
+    {
+        id: 5,
+        title: 'Cardiology',
+        img: 'https://i.ibb.co/f2ZGfFg/img5.jpg',
+        description: 'Cardiology is the study and treatment of disorders of the heart and the blood vessels. A person with heart disease or cardiovascular disease may be referred to a cardiologist. Cardiology is a branch of internal medicine. A cardiologist is not the same as a cardiac surgeon. A cardiac surgeon opens the chest and performs heart surgery.',
+    },
+    {
+        id: 6,
+        title: 'Researches',
+        img: 'https://i.ibb.co/wdXRz4y/img6.jpg',
+        description: 'Medical research involves research in a wide range of fields, such as biology, chemistry, pharmacology and toxicology with the goal of developing new medicines or medical procedures or improving the application of those already available.',
+    }
+]
