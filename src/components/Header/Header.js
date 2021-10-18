26431fc5947a42c7f1e74d7c90b34107fb55f7d8
romjanxr <romjanvr5@gmail.com>
2021-10-19 00:08:08 +0600

    header styles added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+            <Navbar collapseOnSelect expand="lg" className="header" variant="light" sticky="top">
                 <Container>
                     <Navbar.Brand href="#home"><img width="150px" src={logo} alt="docfind" /></Navbar.Brand>
                     <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Collapse className="justify-content-end link">
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/services">Services</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <Navbar.Text>
+                        <Navbar.Text className="link">
                             Signed in as: <a href="#login">Mark Otto</a>
                         </Navbar.Text>
                     </Navbar.Collapse>
